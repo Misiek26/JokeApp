35fdb3871064fa2fcf868fb21cdd3f5d4bca89d8
Laptop-HP <micha.ogorek@op.pl>
2023-06-02 17:54:28 +0200

    add view of categories

--- src/main/java/com/JokeApp/Project/controller/MainController.java
+++ src/main/java/com/JokeApp/Project/controller/MainController.java
@@ -1,13 +1,19 @@
 package com.JokeApp.Project.controller;
 
+import com.JokeApp.Project.model.Category;
+import com.JokeApp.Project.model.Joke;
 import com.JokeApp.Project.model.UserJoke;
+import com.JokeApp.Project.service.CategoryService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -27,6 +33,28 @@ public class MainController {
         ObjectMapper objectMapper = new ObjectMapper();
         List<UserJoke> userJokes = objectMapper.readValue(responseEntity.getBody(), new TypeReference<List<UserJoke>>() {});
         return userJokes;
+    }
 
+    @GetMapping("/categories")
+    public String getCategories(Model model){
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Category[]> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/categories", Category[].class);
+
+        List<Category> categories = List.of(responseEntity.getBody());
+        model.addAttribute("categories", categories);
+        return "categories";
+    }
+
+    @GetMapping("/categories/{categoryName}")
+    public String getJokesByCategory(@PathVariable String categoryName, Model model){
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Joke[]> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/jokes/category?category=" + categoryName, Joke[].class);
+
+        List<Joke> jokes = List.of(responseEntity.getBody());
+        model.addAttribute("jokes", jokes);
+        System.out.println(jokes.get(0).getCategory().getName());
+        return "category-jokes";
     }
 }
